872b86d294f533fece8f3ed52305cd6514a5fe16
vdsalex <alexandre.vandensteen@gmail.com>
2022-09-24 18:34:52 +0200

    initialize project

---
+++ src/main/kotlin/com/vds/wishow/kwebblockchain/KWebBlockchainApplication.kt
@@ -0,0 +1,11 @@
+package com.vds.wishow.kwebblockchain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KWebBlockchainApplication
+
+fun main(args: Array<String>) {
+	runApplication<KWebBlockchainApplication>(*args)
+}
---
+++ src/test/kotlin/com/vds/wishow/kwebblockchain/KWebBlockchainApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vds.wishow.kwebblockchain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KWebBlockchainApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
